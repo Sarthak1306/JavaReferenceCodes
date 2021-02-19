6260aaa0a6ea60d371f50af3853eabb769c32601
Sarthak1306 <sarthak13062001@gmail.com>
2021-02-19 12:11:16 +0530

    Multiplication Table of a number

---
+++ Table of a number/Table.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Table {
+	public static void main(String[] args) {
+		
+		int i=Integer.parseInt(args[0]);
+		for(int k=1;k<=10;k++)
+		{
+			System.out.println(i+"*"+k+"="+(i*k));
+		}
+
+	}
+}
